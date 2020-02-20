474c90117747b532d0d539765c914eae9025f2b6
jorelrx <jnull.rx@gmail.com>
2020-02-20 14:25:57 -0300

    script alterado
    o

--- Jogo2D/Assets/Scripts/NewBehaviourScript.cs
+++ Jogo2D/Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Olá Mundo");
     }
 
     // Update is called once per frame
