492aebd0d0940a3ec164cf84fb30106139a623ce
jorelrx <jnull.rx@gmail.com>
2020-02-21 11:33:39 -0300

    Implemantação de pulo

---
+++ Jogo2D/Assets/Scripts/MovPersonagem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPersonagem : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float forcaPulo = 400;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.position.y <= 1.1f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector2(0, forcaPulo));
+            }
+        }
+    }
+}
